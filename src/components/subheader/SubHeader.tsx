b57e0a17b5da87b17750ddaedb3a41b2ce7f598e
Mark Bolier <markbolier@icloud.com>
2022-09-19 11:31:05 +0200

    Initial commit

--- src/components/subheader/SubHeader.tsx
+++ src/components/subheader/SubHeader.tsx
@@ -1,18 +1,17 @@
-import React, { useState } from "react";
 import * as Styled from "./SubHeader.styled";
 import { Button } from "../button";
 
 interface SubHeaderProps {
   label: string;
-  amount: number;
+  entries: number;
 }
 
-export const SubHeader = ({ label = "Timesheets", amount = 0 }: SubHeaderProps) => {
+export const SubHeader = ({ label = "Timesheets", entries = 0 }: SubHeaderProps) => {
   return (
     <Styled.Container>
       <Styled.Title>{label}</Styled.Title>
       <Styled.Divider>|</Styled.Divider>
-      <Styled.Counter>{amount} Entries</Styled.Counter>
+      <Styled.Counter>{entries} Entries</Styled.Counter>
       <Button plusIcon label="New Time Entry" />
     </Styled.Container>
   );
