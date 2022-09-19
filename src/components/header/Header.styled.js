e33956aa09a88c3b47c72ce45548b6a10c373b78
Mark Bolier <markbolier@icloud.com>
2022-09-19 12:51:23 +0200

    Before merge

--- src/components/header/Header.styled.js
+++ src/components/header/Header.styled.js
@@ -1,5 +1,9 @@
 import styled from "styled-components";
 
+type HeaderProps = {
+  isActive: boolean;
+};
+
 export const Container = styled.nav`
   align-items: center;
   background: ${({ theme }) => theme.backgroundSecondary};
@@ -15,12 +19,11 @@ export const Logo = styled.a`
   font-family: ${({ theme }) => theme.fontSecondary};
   font-size: 26px;
   font-weight: light;
-  margin-left: 0.5em;
-  margin-right: 1.5em;
+  padding: 0 30px;
   white-space: nowrap;
 `;
 
-export const Menu = styled.ul`
+export const Menu = styled.ul<HeaderProps>`
   background: ${({ theme }) => theme.backgroundSecondary};
   display: ${(props) => (props.isActive ? "none" : "block")};
   flex-direction: column;
@@ -59,13 +62,12 @@ export const MenuItems = styled.li`
   }
 `;
 
-export const Toggle = styled.button`
+export const Toggle = styled.button<HeaderProps>`
   color: white;
   cursor: pointer;
   display: ${(props) => (props.isActive ? "none" : "block")};
-  height: 12px;
   margin-left: auto;
-  padding-right: 20px;
+  padding-right: 30px;
 
   @media (${({ theme }) => theme.tablet}) {
     display: none;
--- src/components/subheader/SubHeader.tsx
+++ src/components/subheader/SubHeader.tsx
@@ -4,16 +4,29 @@ import { Button } from "../button";
 
 interface SubHeaderProps {
   label: string;
+<<<<<<< HEAD
   amount: number;
 }
 
 export const SubHeader = ({ label = "Timesheets", amount = 0 }: SubHeaderProps) => {
+=======
+  entries: number;
+  onClick: boolean;
+}
+
+export const SubHeader = ({ label = "Timesheets", entries = 0, onClick }: SubHeaderProps) => {
+>>>>>>> 512d40d (Save for later)
   return (
     <Styled.Container>
       <Styled.Title>{label}</Styled.Title>
       <Styled.Divider>|</Styled.Divider>
+<<<<<<< HEAD
       <Styled.Counter>{amount} Entries</Styled.Counter>
       <Button plusIcon label="New Time Entry" />
+=======
+      <Styled.Counter>{entries} Entries</Styled.Counter>
+      <Button plusIcon label="New Time Entry" onClick={onClick} />
+>>>>>>> 512d40d (Save for later)
     </Styled.Container>
   );
 };
