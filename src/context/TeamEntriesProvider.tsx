cb0041bdad93f3efc94bc7903421d521ec438fa5
Mark Bolier <markbolier@icloud.com>
2022-10-07 14:39:26 +0200

    Added teamEntriesProvider

---
+++ src/context/TeamEntriesProvider.tsx
@@ -0,0 +1,26 @@
+import React, { createContext, useState, Dispatch, SetStateAction, FC, ReactNode } from "react";
+
+import * as Types from "../types/types";
+
+interface TeamEntriesProvider {
+  children: ReactNode;
+  initialTimeEntries: Types.TeamEntry[];
+}
+
+interface TeamEntriesContext {
+  initialTeamEntries?: Types.TeamEntry[];
+  setTeamEntries: Dispatch<SetStateAction<Types.TeamEntry[]>>;
+  teamEntries: Types.TeamEntry[];
+}
+
+export const TeamEntriesContext = createContext<TeamEntriesContext>({} as TeamEntriesContext);
+
+export const TeamEntriesProvider: FC<TeamEntriesProvider> = ({ children, initialTeamEntries }) => {
+  const [teamEntries, setTeamEntries] = useState(initialTeamEntries);
+  const entries = {
+    teamEntries,
+    setTeamEntries,
+  };
+
+  return <TeamEntriesContext.Provider value={entries}>{children}</TeamEntriesContext.Provider>;
+};
