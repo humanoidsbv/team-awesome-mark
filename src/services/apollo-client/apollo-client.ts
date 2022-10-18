eaee7db89f0aa36447b318728c5cf45052522e35
Mark Bolier <markbolier@icloud.com>
2022-10-18 14:48:32 +0200

    Fix for cacheing issue

--- src/services/apollo-client/apollo-client.ts
+++ src/services/apollo-client/apollo-client.ts
@@ -2,6 +2,12 @@ import { ApolloClient, createHttpLink, InMemoryCache } from "@apollo/client";
 
 export const client = new ApolloClient({
   cache: new InMemoryCache(),
+  defaultOptions: {
+    watchQuery: {
+      fetchPolicy: "no-cache",
+      errorPolicy: "ignore",
+    },
+  },
   link: createHttpLink({
     uri: "http://localhost:3333",
   }),
