60ce4beffe5052023a41525b542107bb6dcc5243
Mark Bolier <markbolier@icloud.com>
2022-11-08 15:35:01 +0100

    Upgrade NextJS to v13 and reworked header links to be compatible

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -14,34 +14,32 @@ export const Header = () => {
   return (
     <Styled.Container isActive={isActive}>
       <Link href="/" passHref>
-        <Styled.Logo>team awesome</Styled.Logo>
+        team awesome
       </Link>
       <Styled.Menu isActive={!isActive}>
         <Styled.MenuItems>
-          <Link href="/" passHref>
-            <Styled.Item onClick={handleClick}>Timesheets</Styled.Item>
+          <Link href="/" onClick={handleClick} passHref>
+            Timesheets
           </Link>
         </Styled.MenuItems>
         <Styled.MenuItems>
-          <Link href="/team-members" passHref>
-            <Styled.Item data-cy="team-members-link" onClick={handleClick}>
-              Team members
-            </Styled.Item>
+          <Link href="/team-members" data-cy="team-members-link" onClick={handleClick} passHref>
+            Team members
           </Link>
         </Styled.MenuItems>
         <Styled.MenuItems>
-          <Link href="/" passHref>
-            <Styled.Item onClick={handleClick}>Projects</Styled.Item>
+          <Link href="/" onClick={handleClick} passHref>
+            Projects
           </Link>
         </Styled.MenuItems>
         <Styled.MenuItems>
-          <Link href="/" passHref>
-            <Styled.Item onClick={handleClick}>Clients</Styled.Item>
+          <Link href="/" onClick={handleClick} passHref>
+            Clients
           </Link>
         </Styled.MenuItems>
         <Styled.MenuItems>
-          <Link href="/" passHref>
-            <Styled.Item onClick={handleClick}>Documents</Styled.Item>
+          <Link href="/" onClick={handleClick} passHref>
+            Documents
           </Link>
         </Styled.MenuItems>
       </Styled.Menu>
